35cb3806e7962edf56b0f30fcf0e7999878ffd03
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-23 13:40:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -963,103 +963,314 @@ function functionWithArgs (param1, param2) {
   }
   
   functionWithArgs(1,2)
-// ___________________________________________
+
+// ___________________________________________ 51 Return a Value from a Function with Return
 
 /*
+We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
 
+Example
+
+function plusThree(num) {
+  return num + 3;
+}
+
+const answer = plusThree(5);
+answer has the value 8.
+
+plusThree takes an argument for num and returns a value equal to num + 3.
+
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
  */
-
-// ___________________________________________
+function timesFive (num) {
+  return num * 5
+}
+// ___________________________________________ 52 Global Scope and Functions
 
 /*
+In JavaScript, scope refers to the visibility of variables. Variables which are defined outside of a function block have Global scope. This means, they can be seen everywhere in your JavaScript code.
 
+Variables which are declared without the let or const keywords are automatically created in the global scope. This can create unintended consequences elsewhere in your code or when running a function again. You should always declare your variables with let or const.
+
+Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+
+Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
  */
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
 
-// ___________________________________________
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+// ___________________________________________ 53 Local Scope and Functions
+
 
 /*
+Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
 
+Here is a function myTest with a local variable called loc.
+
+function myTest() {
+  const loc = "foo";
+  console.log(loc);
+}
+
+myTest();
+console.log(loc);
+The myTest() function call will display the string foo in the console. The console.log(loc) line (outside of the myTest function) will throw an error, as loc is not defined outside of the function.
+
+The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
  */
+function myLocalScope() {
+  // Only change code below this line
+  const myVar = 'some'
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
 
-// ___________________________________________
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+// ___________________________________________ 54 Global vs. Local Scope in Functions
 
 /*
+It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
 
+In this example:
+
+const someVar = "Hat";
+
+function myFun() {
+  const someVar = "Head";
+  return someVar;
+}
+The function myFun will return the string Head because the local version of the variable is present.
+
+Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
  */
+// Setup
+const outerWear = "T-Shirt";
 
-// ___________________________________________
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = 'sweater'
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+// ___________________________________________ 55 Understanding Undefined Value returned from a Function
+
 
 /*
+A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
 
+Example
+
+let sum = 0;
+
+function addSum(num) {
+  sum = sum + num;
+}
+
+addSum(3);
+addSum is a function without a return statement. The function will change the global sum variable but the returned value of the function is undefined.
+
+Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
  */
+// Setup
+let sum = 0;
 
-// ___________________________________________
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum += 5;
+}
+// Only change code above this line
+
+addThree();
+addFive();
+// ___________________________________________ 56 Assignment with a Returned Value
 
 /*
+If you'll recall from our discussion about Storing Values with the Assignment Operator, everything to the right of the equal sign is resolved before the value is assigned. This means we can take the return value of a function and assign it to a variable.
 
+Assume we have defined a function sum which adds two numbers together.
+
+ourSum = sum(5, 12);
+Calling the sum function with the arguments of 5 and 12 produces a return value of 17. This return value is assigned to the ourSum variable.
+
+Call the processArg function with an argument of 7 and assign its return value to the variable processed.
  */
+// Setup
+let processed = 0;
 
-// ___________________________________________
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7)
+// Only change code below this line
+
+// ___________________________________________ 57 Stand in Line
 
 /*
+In Computer Science a queue is an abstract Data Structure where items are kept in order. New items can be added at the back of the queue and old items are taken off from the front of the queue.
 
+Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+Add the number to the end of the array, then remove the first element of the array.
+
+The nextInLine function should then return the element that was removed.
  */
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item)
+  const deletedElement = arr.shift()
+  return deletedElement;
+  // Only change code above this line
+}
 
-// ___________________________________________
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+// ___________________________________________ 58 Understanding Boolean Values
 
 /*
+Another data type is the Boolean. Booleans may only be one of two values: true or false. They are basically little on-off switches, where true is on and false is off. These two states are mutually exclusive.
+
+Note: Boolean values are never written with quotes. The strings "true" and "false" are not Boolean and have no special meaning in JavaScript.
+
+Modify the welcomeToBooleans function so that it returns true instead of false.
+
 
  */
+function welcomeToBooleans() {
+  // Only change code below this line
 
-// ___________________________________________
+  return true; // Change this line
+
+  // Only change code above this line
+}
+// ___________________________________________ 59 Use Conditional Logic with If Statements
 
 /*
+if statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
 
+When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
+
+Pseudocode
+
+if (condition is true) {
+  statement is executed
+}
+Example
+
+function test (myCondition) {
+  if (myCondition) {
+    return "It was true";
+  }
+  return "It was false";
+}
+
+test(true);
+test(false);
+test(true) returns the string It was true, and test(false) returns the string It was false.
+
+When test is called with a value of true, the if statement evaluates myCondition to see if it is true or not. Since it is true, the function returns It was true. When we call test with a value of false, myCondition is not true and the statement in the curly braces is not executed and the function returns It was false.
+
+Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
  */
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if (wasThatTrue) {
+    return 'Yes, that was true'
+  } else {
+    return 'No, that was false'
+  }
 
-// ___________________________________________
+
+  // Only change code above this line
+
+}
+// ___________________________________________ 60 Comparison with the Equality Operator
 
 /*
+There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.
 
+The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're equivalent or false if they are not. Note that equality is different from assignment (=), which assigns the value on the right of the operator to a variable on the left.
+
+function equalityTest(myVal) {
+  if (myVal == 10) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+If myVal is equal to 10, the equality operator returns true, so the code in the curly braces will execute, and the function will return Equal. Otherwise, the function will return Not Equal. In order for JavaScript to compare two different data types (for example, numbers and strings), it must convert one type to another. This is known as Type Coercion. Once it does, however, it can compare terms as follows:
+
+1   ==  1  // true
+1   ==  2  // false
+1   == '1' // true
+"3" ==  3  // true
+Add the equality operator to the indicated line so that the function will return the string Equal when val is equivalent to 12.
  */
+// Setup
+function testEqual(val) {
+  if (val == 12 ) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
 
-// ___________________________________________
+testEqual(10);
+// ___________________________________________ 61 Comparison with the Strict Equality Operator
 
 /*
+Strict equality (===) is the counterpart to the equality operator (==). However, unlike the equality operator, which attempts to convert both values being compared to a common type, the strict equality operator does not perform a type conversion.
 
+If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
+
+Examples
+
+3 ===  3  // true
+3 === '3' // false
+In the second example, 3 is a Number type and '3' is a String type.
+
+Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7.
  */
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
 
-// ___________________________________________
-
-/*
-
- */
-
-// ___________________________________________
-
-/*
-
- */
-
-// ___________________________________________
-
-/*
-
- */
-
-// ___________________________________________
-
-/*
-
- */
-
-// ___________________________________________
-
-/*
-
- */
-
-// ___________________________________________
+testStrict(10);
+// ___________________________________________ 62 Practice comparing different values
 
 /*
 
