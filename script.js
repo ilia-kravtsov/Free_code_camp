003e490f39fa71d400bfc01bce8a9323e958da1c
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-26 17:17:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1931,6 +1931,444 @@ function isLess(a, b) {
 }
 
 isLess(10, 15);
+// ___________________________________________ 81 Return Early Pattern for Functions
+
+/*
+When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+Example
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+
+Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+
+Hint
+Remember that undefined is a keyword, not a string.
+*/
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined
+  }
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+// ___________________________________________ 82 Counting Cards
+
+/*
+In the casino game Blackjack, a player can determine whether they have an advantage on the next hand over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+
+Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+
+Count Change	Cards
++1	2, 3, 4, 5, 6
+0	7, 8, 9
+-1	10, 'J', 'Q', 'K', 'A'
+You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+Example Outputs: -3 Hold or 5 Bet
+
+Hint
+Do NOT reset count to 0 when value is 7, 8, or 9.
+Do NOT return an array.
+Do NOT include quotes (single or double) in the output.
+*/
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--
+      break;
+    default:
+      count
+      break;
+  }
+  return `${count} ${count > 0 ? 'Bet' : 'Hold'}`
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+// ___________________________________________ 83 Build JavaScript Objects
+
+/*
+You may have heard the term object before.
+
+Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+Here's a sample cat object:
+
+const cat = {
+  "name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+};
+In this example, all the properties are stored as strings, such as name, legs, and tails. However, you can also use numbers as properties. You can even omit the quotes for single-word string properties, as follows:
+
+const anotherObject = {
+  make: "Ford",
+  5: "five",
+  "model": "focus"
+};
+However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+*/
+const myDog = {
+  // Only change code below this line
+  name: 'Sharik',
+  legs: 4,
+  tails: 1,
+  friends: ['Tobik', 'Rex']
+  // Only change code above this line
+};
+// ___________________________________________ 84 Accessing Object Properties with Dot Notation
+
+/*
+There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
+
+Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
+
+Here is a sample of using dot notation (.) to read an object's property:
+
+const myObj = {
+  prop1: "val1",
+  prop2: "val2"
+};
+
+const prop1val = myObj.prop1;
+const prop2val = myObj.prop2;
+prop1val would have a value of the string val1, and prop2val would have a value of the string val2.
+
+Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+*/
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+// ___________________________________________ 85 Accessing Object Properties with Bracket Notation
+
+/*
+The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
+
+However, you can still use bracket notation on object properties without spaces.
+
+Here is a sample of using bracket notation to read an object's property:
+
+const myObj = {
+  "Space Name": "Kirk",
+  "More Space": "Spock",
+  "NoSpace": "USS Enterprise"
+};
+
+myObj["Space Name"];
+myObj['More Space'];
+myObj["NoSpace"];
+myObj["Space Name"] would be the string Kirk, myObj['More Space'] would be the string Spock, and myObj["NoSpace"] would be the string USS Enterprise.
+
+Note that property names with spaces in them must be in quotes (single or double).
+
+Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+*/
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj['an entree'];   // Change this line
+const drinkValue = testObj['the drink'];    // Change this line
+// ___________________________________________ 86 Accessing Object Properties with Variables
+
+/*
+Another use of bracket notation on objects is to access a property which is stored as the value of a variable. This can be very useful for iterating through an object's properties or when accessing a lookup table.
+
+Here is an example of using a variable to access a property:
+
+const dogs = {
+  Fido: "Mutt",
+  Hunter: "Doberman",
+  Snoopie: "Beagle"
+};
+
+const myDog = "Hunter";
+const myBreed = dogs[myDog];
+console.log(myBreed);
+The string Doberman would be displayed in the console.
+
+Note that we do not use quotes around the variable name when using it to access the property because we are using the value of the variable, not the name.
+
+Set the playerNumber variable to 16. Then, use the variable to look up the player's name and assign it to player.
+*/
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+// ___________________________________________ 87 Updating Object Properties
+
+/*
+After you've created a JavaScript object, you can update its properties at any time just like you would update any other variable. You can use either dot or bracket notation to update.
+
+For example, let's look at ourDog:
+
+const ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+Since he's a particularly happy dog, let's change his name to the string Happy Camper. Here's how we update his object's name property: ourDog.name = "Happy Camper"; or ourDog["name"] = "Happy Camper"; Now when we evaluate ourDog.name, instead of getting Camper, we'll get his new name, Happy Camper.
+
+Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+*/
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = 'Happy Coder'
+// Only change code below this line
+
+// ___________________________________________ 88 Add New Properties to a JavaScript Object
+/*
+You can add new properties to existing JavaScript objects the same way you would modify them.
+
+Here's how we would add a bark property to ourDog:
+
+ourDog.bark = "bow-wow";
+or
+
+ourDog["bark"] = "bow-wow";
+Now when we evaluate ourDog.bark, we'll get his bark, bow-wow.
+
+Example:
+
+const ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+*/
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = 'woof'
+
+// ___________________________________________ 89 Delete Properties from a JavaScript Object
+
+/*
+We can also delete properties from objects like this:
+
+delete ourDog.bark;
+Example:
+
+const ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+After the last line shown above, ourDog looks like:
+
+{
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+}
+Delete the tails property from myDog. You may use either dot or bracket notation.
+*/
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails
+// Only change code below this line
+
+// ___________________________________________ 90 Using Objects for Lookups
+
+/*
+Objects can be thought of as a key/value storage, like a dictionary. If you have tabular data, you can use an object to lookup values rather than a switch statement or an if/else chain. This is most useful when you know that your input data is limited to a certain range.
+
+Here is an example of an article object:
+
+const article = {
+  "title": "How to create objects in JavaScript",
+  "link": "https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/",
+  "author": "Kaashan Hussain",
+  "language": "JavaScript",
+  "tags": "TECHNOLOGY",
+  "createdAt": "NOVEMBER 28, 2018"
+};
+
+const articleAuthor = article["author"];
+const articleLink = article["link"];
+
+const value = "title";
+const valueLookup = article[value];
+articleAuthor is the string Kaashan Hussain, articleLink is the string https://www.freecodecamp.org/news/a-complete-guide-to-creating-objects-in-javascript-b0e2450655e8/, and valueLookup is the string How to create objects in JavaScript.
+
+Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+*/
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  switch(val) {
+    case "alpha":
+      result = "Adams";
+      break;
+    case "bravo":
+      result = "Boston";
+      break;
+    case "charlie":
+      result = "Chicago";
+      break;
+    case "delta":
+      result = "Denver";
+      break;
+    case "echo":
+      result = "Easy";
+      break;
+    case "foxtrot":
+      result = "Frank";
+  }
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  }
+  result = lookup[val]
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+// ___________________________________________ 91 Testing Objects for Properties
+
+/*
+To check if a property on a given object exists or not, you can use the .hasOwnProperty() method. someObject.hasOwnProperty(someProperty) returns true or false depending on if the property is found on the object or not.
+
+Example
+
+function checkForProperty(object, property) {
+  return object.hasOwnProperty(property);
+}
+
+checkForProperty({ top: 'hat', bottom: 'pants' }, 'top'); // true
+checkForProperty({ top: 'hat', bottom: 'pants' }, 'middle'); // false
+The first checkForProperty function call returns true, while the second returns false.
+
+Modify the function checkObj to test if the object passed to the function parameter obj contains the specific property passed to the function parameter checkProp. If the property passed to checkProp is found on obj, return that property's value. If not, return Not Found.
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+
 // ___________________________________________
 
 /*
@@ -1965,171 +2403,299 @@ isLess(10, 15);
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
-// ___________________________________________
-
-/*
-
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
 
 // ___________________________________________
 
 /*
 
- */
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
+// ___________________________________________
+
+/*
+
+*/
+
