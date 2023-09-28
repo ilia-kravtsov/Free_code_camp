268a9b7c4c75f25bf9d45a31d9625363726f87c4
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-28 13:51:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2734,72 +2734,326 @@ In this case, we initialize the value of i to 5, just like we did with the while
 
 Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
 */
+// Setup
+const myArray = [];
+let i = 10;
 
-// ___________________________________________
-
-/*
-
-*/
-
-// ___________________________________________
-
-/*
-
-*/
-
-// ___________________________________________
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+}
+while (i < 10) 
+// ___________________________________________ 103 Replace Loops using Recursion
 
 /*
+Recursion is the concept that a function can be expressed in terms of itself. To help understand this, start by thinking about the following task: multiply the first n elements of an array to create the product of those elements. Using a for loop, you could do this:
 
+  function multiply(arr, n) {
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+    return product;
+  }
+However, notice that multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That means you can rewrite multiply in terms of itself and never need to use a loop.
+
+  function multiply(arr, n) {
+    if (n <= 0) {
+      return 1;
+    } else {
+      return multiply(arr, n - 1) * arr[n - 1];
+    }
+  }
+The recursive version of multiply breaks down like this. In the base case, where n <= 0, it returns 1. For larger values of n, it calls itself, but with n - 1. That function call is evaluated in the same way, calling multiply again until n <= 0. At this point, all the functions can return and the original multiply returns the answer.
+
+Note: Recursive functions must have a base case when they return without calling the function again (in this example, when n <= 0), otherwise they can never finish executing.
+
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
 */
-
-// ___________________________________________
+function sum(arr, n) {
+  // Only change code below this line
+    if (n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  // Only change code above this line
+}
+// ___________________________________________ 104 Profile Lookup
 
 /*
+We have an array of objects representing different people in our contacts lists.
 
+A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+
+The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+
+If both are true, then return the "value" of that property.
+
+If name does not correspond to any contacts then return the string No such contact.
+
+If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
 */
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
 
-// ___________________________________________
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+    let result;
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name && contacts[i].hasOwnProperty(prop)) {
+            result = contacts[i][prop]
+            return result
+        } else if (contacts[i].firstName !== name) {
+            result = 'No such contact'
+        } else if (!contacts[i].hasOwnProperty(prop)) {
+            result = 'No such property'
+            return result
+        }
+    }
+    return result
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
+// ___________________________________________ 105 Generate Random Fractions with JavaScript
 
 /*
+Random numbers are useful for creating random behavior.
 
+JavaScript has a Math.random() function that generates a random decimal number between 0 (inclusive) and 1 (exclusive). Thus Math.random() can return a 0 but never return a 1.
+
+Note: Like Storing Values with the Assignment Operator, all function calls will be resolved before the return executes, so we can return the value of the Math.random() function.
+
+Change randomFraction to return a random number instead of returning 0.
 */
+function randomFraction() {
 
-// ___________________________________________
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
+// ___________________________________________ 106 Generate Random Whole Numbers with JavaScript
 
 /*
+You can generate random decimal numbers with Math.random(), but sometimes you need to generate random whole numbers. The following process will give you a random whole number less than 20:
 
+Use Math.random() to generate a random decimal number.
+Multiply that random decimal number by 20.
+Use Math.floor() to round this number down to its nearest whole number.
+Remember that Math.random() can never quite return a 1, so it's impossible to actually get 20 since you are rounding down with Math.floor(). This process will give you a random whole number in the range from 0 to 19.
+
+Putting everything together, this is what your code looks like:
+
+Math.floor(Math.random() * 20);
+You are calling Math.random(), multiplying the result by 20, then passing the value to Math.floor() to round the value down to the nearest whole number.
+
+Use this technique to generate and return a random whole number in the range from 0 to 9.
 */
-
-// ___________________________________________
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+// ___________________________________________ 107 Generate Random Whole Numbers within a Range
 
 /*
+You can generate a random whole number in the range from zero to a given number. You can also pick a different lower number for this range.
 
+You'll call your minimum number min and your maximum number max.
+
+This formula gives a random whole number in the range from min to max. Take a moment to read it and try to understand what this code is doing:
+
+Math.floor(Math.random() * (max - min + 1)) + min
+Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin and less than or equal to myMax.
 */
-
-// ___________________________________________
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
+}
+// ___________________________________________ 108 Use the parseInt Function
 
 /*
+The parseInt() function parses a string and returns an integer. Here's an example:
 
+const a = parseInt("007");
+The above function converts the string 007 to the integer 7. If the first character in the string can't be converted into a number, then it returns NaN.
+
+Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
 */
+function convertToInteger(str) {
+  return parseInt(str)
+}
 
-// ___________________________________________
+convertToInteger("56");
+// ___________________________________________ 109 Use the parseInt Function with a Radix
 
 /*
+Use the parseInt Function with a Radix
+The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
 
+The function call looks like:
+
+parseInt(string, radix);
+And here's an example:
+
+const a = parseInt("11", 2);
+The radix variable says that 11 is in the binary system, or base 2. This example converts the string 11 to an integer 3.
+
+Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
 */
+function convertToInteger(str) {
+  return parseInt(str, 2)
+}
 
-// ___________________________________________
+convertToInteger("10011");
+// ___________________________________________ 110 Use the Conditional (Ternary) Operator
 
 /*
+The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
 
+The syntax is a ? b : c, where a is the condition, b is the code to run when the condition returns true, and c is the code to run when the condition returns false.
+
+The following function uses an if/else statement to check a condition:
+
+function findGreater(a, b) {
+  if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater or equal";
+  }
+}
+This can be re-written using the conditional operator:
+
+function findGreater(a, b) {
+  return a > b ? "a is greater" : "b is greater or equal";
+}
+Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either the string Equal or the string Not Equal.
 */
+function checkEqual(a, b) {
+  return a === b ? 'Equal' : 'Not Equal';
+}
 
-// ___________________________________________
+checkEqual(1, 2);
+// ___________________________________________ 111 Use Multiple Conditional (Ternary) Operators
 
 /*
+In the previous challenge, you used a single conditional operator. You can also chain them together to check for multiple conditions.
 
+The following function uses if, else if, and else statements to check multiple conditions:
+
+function findGreaterOrEqual(a, b) {
+  if (a === b) {
+    return "a and b are equal";
+  }
+  else if (a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+The above function can be re-written using multiple conditional operators:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" 
+    : (a > b) ? "a is greater" 
+    : "b is greater";
+}
+It is considered best practice to format multiple conditional operators such that each condition is on a separate line, as shown above. Using multiple conditional operators without proper indentation may make your code hard to read. For example:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
+In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
 */
+function checkSign(num) {
+  return num > 0 ? 'positive' : num < 0 ? 'negative' : 'zero'
+}
+
+checkSign(10);
+// ___________________________________________ 112 Use Recursion to Create a Countdown
+ 
+/*
+In a previous challenge, you learned how to use recursion to replace a for loop. Now, let's look at a more complex function that returns an array of consecutive integers starting with 1 through the number passed to the function.
+
+As mentioned in the previous challenge, there will be a base case. The base case tells the recursive function when it no longer needs to call itself. It is a simple case where the return value is already known. There will also be a recursive call which executes the original function with different arguments. If the function is written correctly, eventually the base case will be reached.
+
+For example, say you want to write a recursive function that returns an array containing the numbers 1 through n. This function will need to accept an argument, n, representing the final number. Then it will need to call itself with progressively smaller values of n until it reaches 1. You could write the function as follows:
+
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+The value [1, 2, 3, 4, 5] will be displayed in the console.
+
+At first, this seems counterintuitive since the value of n decreases, but the values in the final array are increasing. This happens because the push happens last, after the recursive call has returned. At the point where n is pushed into the array, countup(n - 1) has already been evaluated and returned [1, 2, ..., n - 1].
+
+We have defined a function called countdown with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter. If the function is called with a number less than 1, the function should return an empty array. For example, calling this function with n = 5 should return the array [5, 4, 3, 2, 1]. Your function must use recursion by calling itself and must not use loops of any kind.
+*/
+// Only change code below this line
+function countdown(n){
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+// Only change code above this line
+
+// ___________________________________________ 113 Use Recursion to Create a Range of Numbers
+
+/*
+Continuing from the previous challenge, we provide you another opportunity to create a recursive function to solve a problem.
+
+We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+*/
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum]
+  } else {
+    const range = rangeOfNumbers(startNum, endNum - 1)
+    range.push(endNum)
+    return range
+  }
+};
+
+// _____________________________________________________________ ES6
+// *************************************************************
 
 // ___________________________________________
 
