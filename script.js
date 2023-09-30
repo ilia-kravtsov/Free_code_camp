0b0b0df4ca7bbb981db39dcb534c4e1d7a5e6ffa
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-30 11:21:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3412,60 +3412,321 @@ const LOCAL_FORECAST = {
 const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
 
 // Only change code above this line
-// ___________________________________________
+// ___________________________________________ 12 Use Destructuring Assignment to Assign Variables from Arrays
 
 /*
+ES6 makes destructuring arrays as easy as destructuring objects.
 
+One key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma-separated list. Consequently, you cannot pick or choose which elements you want to assign to variables.
+
+Destructuring an array lets us do exactly that:
+
+const [a, b] = [1, 2, 3, 4, 5, 6];
+console.log(a, b);
+The console will display the values of a and b as 1, 2.
+
+The variable a is assigned the first value of the array, and b is assigned the second value of the array. We can also access the value at any index in an array with destructuring by using commas to reach the desired index:
+
+const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
+console.log(a, b, c);
+The console will display the values of a, b, and c as 1, 2, 5.
+
+Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
 */
-
-// ___________________________________________
+let a = 8, b = 6;
+// Only change code below this line
+[b,a] = [a,b]
+// ___________________________________________ 13 Destructuring via rest elements
 
 /*
+In some situations involving array destructuring, we might want to collect the rest of the elements into a separate array.
 
+The result is similar to Array.prototype.slice(), as shown below:
+
+const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+console.log(a, b);
+console.log(arr);
+The console would display the values 1, 2 and [3, 4, 5, 7].
+
+Variables a and b take the first and second values from the array. After that, because of the rest syntax presence, arr gets the rest of the values in the form of an array. The rest element only works correctly as the last variable in the list. As in, you cannot use the rest syntax to catch a subarray that leaves out the last element of the original array.
+
+Use a destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() should return a sub-array of the original array list with the first two elements omitted.
 */
+function removeFirstTwo(list) {
+    let [a,b,...rest] = list
+    return rest;
+}
 
-// ___________________________________________
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+// ___________________________________________ 14 Use Destructuring Assignment to Pass an Object as a Function's Parameters
 
 /*
+In some cases, you can destructure the object in a function argument itself.
 
+Consider the code below:
+
+const profileUpdate = (profileData) => {
+  const { name, age, nationality, location } = profileData;
+
+}
+This effectively destructures the object sent into the function. This can also be done in-place:
+
+const profileUpdate = ({ name, age, nationality, location }) => {
+
+}
+When profileData is passed to the above function, the values are destructured from the function parameter for use within the function.
+
+Use destructuring assignment within the argument to the function half to send only max and min inside the function.
 */
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
 
-// ___________________________________________
+// Only change code below this line
+const half = ({max,min}) => (max + min) / 2.0;
+// Only change code above this line
+// ___________________________________________ 15 Create Strings using Template Literals
 
 /*
+A new feature of ES6 is the template literal. This is a special type of string that makes creating complex strings easier.
 
+Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
+
+Consider the code below:
+
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56
+};
+
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+The console will display the strings Hello, my name is Zodiac Hasbro! and I am 56 years old..
+
+A lot of things happened there. Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string. Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting \n within strings. The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. Similarly, you can include other expressions in your string literal, for example ${a + b}. This new way of creating strings gives you more flexibility to create robust strings.
+
+Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+
+Use an iterator method (any kind of loop) to get the desired output (shown below).
+
+[
+  '<li class="text-warning">no-var</li>',
+  '<li class="text-warning">var-on-top</li>',
+  '<li class="text-warning">linebreak</li>'
+]
 */
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = arr.map((f,i) => `<li class="text-warning">${f}</li>`)
+  // Only change code above this line
 
-// ___________________________________________
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+// ___________________________________________ 16 Write Concise Object Literal Declarations Using Object Property Shorthand
 
 /*
+ES6 adds some nice support for easily defining object literals.
 
+Consider the following code:
+
+const getMousePosition = (x, y) => ({
+  x: x,
+  y: y
+});
+getMousePosition is a simple function that returns an object containing two properties. ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood. Here is the same function from above rewritten to use this new syntax:
+
+const getMousePosition = (x, y) => ({ x, y });
+Use object property shorthand with object literals to create and return an object with name, age and gender properties.
 */
-
-// ___________________________________________
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
+// ___________________________________________ 17 Write Concise Declarative Functions with ES6
 
 /*
+When defining functions within objects in ES5, we have to use the keyword function as follows:
 
+const person = {
+  name: "Taylor",
+  sayHello: function() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+With ES6, you can remove the function keyword and colon altogether when defining functions in objects. Here's an example of this syntax:
+
+const person = {
+  name: "Taylor",
+  sayHello() {
+    return `Hello! My name is ${this.name}.`;
+  }
+};
+Refactor the function setGear inside the object bicycle to use the shorthand syntax described above.
 */
-
-// ___________________________________________
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+// ___________________________________________ 18 Use class Syntax to Define a Constructor Function
 
 /*
+ES6 provides a new syntax to create objects, using the class keyword.
 
+In ES5, an object can be created by defining a constructor function and using the new keyword to instantiate the object.
+
+In ES6, a class declaration has a constructor method that is invoked with the new keyword. If the constructor method is not explicitly defined, then it is implicitly defined with no arguments.
+
+// Explicit constructor
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+  }
+  takeOff() {
+    console.log("To " + this.targetPlanet + "!");
+  }
+}
+
+// Implicit constructor
+class Rocket {
+  launch() {
+    console.log("To the moon!");
+  }
+}
+
+const zeus = new SpaceShuttle('Jupiter');
+// prints To Jupiter! in console
+zeus.takeOff();
+
+const atlas = new Rocket();
+// prints To the moon! in console
+atlas.launch();
+It should be noted that the class keyword declares a new function, to which a constructor is added. This constructor is invoked when new is called to create a new object.
+
+Note: UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above.
+
+The constructor method is a special method for creating and initializing an object created with a class. You will learn more about it in the Object Oriented Programming section of the JavaScript Algorithms And Data Structures Certification.
+
+Use the class keyword and write a constructor to create the Vegetable class.
+
+The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
 */
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
 
-// ___________________________________________
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+// ___________________________________________ 19 Use getters and setters to Control Access to an Object
 
 /*
+You can obtain values from an object and set the value of a property within an object.
 
+These are classically called getters and setters.
+
+Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable.
+
+Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. This change could involve calculations, or even overwriting the previous value completely.
+
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+const novel = new Book('anonymous');
+console.log(novel.writer);
+novel.writer = 'newAuthor';
+console.log(novel.writer);
+The console would display the strings anonymous and newAuthor.
+
+Notice the syntax used to invoke the getter and setter. They do not even look like functions. Getters and setters are important because they hide internal implementation details.
+
+Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
+
+Use the class keyword to create a Thermostat class. The constructor accepts a Fahrenheit temperature.
+
+In the class, create a getter to obtain the temperature in Celsius and a setter to set the temperature in Celsius.
+
+Remember that C = 5/9 * (F - 32) and F = C * 9.0 / 5 + 32, where F is the value of temperature in Fahrenheit, and C is the value of the same temperature in Celsius.
+
+Note: When you implement this, you will track the temperature inside the class in one scale, either Fahrenheit or Celsius.
+
+This is the power of a getter and a setter. You are creating an API for another user, who can get the correct result regardless of which one you track.
+
+In other words, you are abstracting implementation details from the user.
 */
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this._temperature = fahrenheit;
+  }
+  get temperature() {
+    return 5/9 * (this._temperature - 32);
+  }
+  set temperature(celsius) {
+    this._temperature = celsius * 9/5 + 32;
+  }
+}
+// Only change code above this line
 
-// ___________________________________________
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+// ___________________________________________ 20 Create a Module Script
 
 /*
+JavaScript started with a small role to play on an otherwise mostly HTML web. Today, it’s huge, and some websites are built almost entirely with JavaScript. In order to make JavaScript more modular, clean, and maintainable; ES6 introduced a way to easily share code among JavaScript files. This involves exporting parts of a file for use in one or more other files, and importing the parts you need, where you need them. In order to take advantage of this functionality, you need to create a script in your HTML document with a type of module. Here’s an example:
 
+<script type="module" src="filename.js"></script>
+A script that uses this module type can now use the import and export features you will learn about in the upcoming challenges.
+
+Add a script to the HTML document of type module and give it the source file of index.js
 */
-
+<html>
+<body>
+<!-- Only change code below this line -->
+<script type="module" src="index.js"></script>
+<!-- Only change code above this line -->
+</body>
+</html>
 // ___________________________________________
 
 /*
