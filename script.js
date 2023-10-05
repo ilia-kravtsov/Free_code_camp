e6b9ad449d95d1b0f10d8e197585fb08b00338d7
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-05 19:59:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4501,29 +4501,96 @@ let checkPass = /(?=\w{3,6})(?=\D*\d)/;
 checkPass.test(password);
 Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
 */
-
-// ___________________________________________ 
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=.*\d{2})/; // Change this line
+let result = pwRegex.test(sampleWord);
+// ___________________________________________ 31 Check For Mixed Grouping of Characters
 
 /*
+Sometimes we want to check for groups of characters using a Regular Expression and to achieve that we use parentheses ().
 
+If you want to find either Penguin or Pumpkin in a string, you can use the following Regular Expression: /P(engu|umpk)in/g
+
+Then check whether the desired string groups are in the test string by using the test() method.
+
+let testStr = "Pumpkin";
+let testRegex = /P(engu|umpk)in/;
+testRegex.test(testStr);
+The test method here would return true.
+
+Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
+
+Then fix the code so that the regex that you have created is checked against myString and either true or false is returned depending on whether the regex matches.
 */
-
-// ___________________________________________ 
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin D.|Eleanor) Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+// ___________________________________________ 32 Reuse Patterns Using Capture Groups
 
 /*
+Say you want to match a word that occurs multiple times like below.
 
+let repeatStr = "row row row your boat";
+You could use /row row row/, but what if you don't know the specific word repeated? Capture groups can be used to find repeated substrings.
+
+Capture groups are constructed by enclosing the regex pattern to be captured in parentheses. In this case, the goal is to capture a word consisting of alphanumeric characters so the capture group will be \w+ enclosed by parentheses: /(\w+)/.
+
+The substring matched by the group is saved to a temporary "variable", which can be accessed within the same regex using a backslash and the number of the capture group (e.g. \1). Capture groups are automatically numbered by the position of their opening parentheses (left to right), starting at 1.
+
+The example below matches a word that occurs thrice separated by spaces:
+
+let repeatRegex = /(\w+) \1 \1/;
+repeatRegex.test(repeatStr); // Returns true
+repeatStr.match(repeatRegex); // Returns ["row row row", "row"]
+Using the .match() method on a string will return an array with the matched substring, along with its captured groups.
+
+Use capture groups in reRegex to match a string that consists of only the same number repeated exactly three times separated by single spaces.
 */
-
-// ___________________________________________ 
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/; // Change this line
+let result = reRegex.test(repeatNum);
+// ___________________________________________ 33 Use Capture Groups to Search and Replace
 
 /*
+Searching is useful. However, you can make searching even more powerful when it also changes (or replaces) the text you match.
 
+You can search and replace text in a string using .replace() on a string. The inputs for .replace() is first the regex pattern you want to search for. The second parameter is the string to replace the match or a function to do something.
+
+let wrongText = "The sky is silver.";
+let silverRegex = /silver/;
+wrongText.replace(silverRegex, "blue");
+The replace call would return the string The sky is blue..
+
+You can also access capture groups in the replacement string with dollar signs ($).
+
+"Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1');
+The replace call would return the string Camp Code.
+
+Write a regex fixRegex using three capture groups that will search for each word in the string one two three. Then update the replaceText variable to replace one two three with the string three two one and assign the result to the result variable. Make sure you are utilizing capture groups in the replacement string using the dollar sign ($) syntax.
 */
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
 
-// ___________________________________________ 
+
+// ________________________________________________________________________________ Debugging
+// ********************************************************************************
+
+// ___________________________________________ 1 Use the JavaScript Console to Check the Value of a Variable
 
 /*
+Both Chrome and Firefox have excellent JavaScript consoles, also known as DevTools, for debugging your JavaScript.
 
+You can find Developer tools in your Chrome's menu or Web Console in Firefox's menu. If you're using a different browser, or a mobile phone, we strongly recommend switching to desktop Firefox or Chrome.
+
+The console.log() method, which "prints" the output of what's within its parentheses to the console, will likely be the most helpful debugging tool. Placing it at strategic points in your code can show you the intermediate values of variables. It's good practice to have an idea of what the output should be before looking at what it is. Having check points to see the status of your calculations throughout your code will help narrow down where the problem is.
+
+Here's an example to print the string Hello world! to the console:
+
+console.log('Hello world!');
+Use the console.log() method to print the value of the variable a where noted in the code.
 */
 
 // ___________________________________________ 
