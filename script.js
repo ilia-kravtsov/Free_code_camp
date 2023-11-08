df802bb23432ad92d7b68c2644e403ac8e0d66e1
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-11-08 18:52:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7841,12 +7841,37 @@ function destroyer(array) {
   return arguments[0].filter(x => !extra.includes(x));
 }
 destroyer([1, 2, 3, 1, 2, 3], 2, 3)
-// ___________________________________________
+// ___________________________________________ Almost Even
+
 
 /*
+We need the ability to divide an unknown integer into a given number of even parts - or at least as even as they can be. The sum of the parts should be the original value, but each part should be an integer, and they should be as close as possible.
 
+Complete the function so that it returns an array of integers representing the parts. If the input number is too small to split it into requested amount of parts, the additional parts should have value 0. Ignoring the order of the parts, there is only one valid solution for each input to your function!
+
+Example:
+splitInteger(20, 6)  // returns [3, 3, 3, 3, 4, 4]
+Inputs
+The input to your function will always be valid for this kata.
 */
+var splitInteger = function(num, parts) {
+  var quotient = Math.floor(num / parts); 
+  var remainder = num % parts; 
 
+  var result = []; // Array to store the parts
+
+  // Fill the array with the quotient value
+  for (var i = 0; i < parts; i++) {
+    result.push(quotient);
+  }
+
+  // Distribute the remainder evenly among the parts
+  for (var j = 0; j < remainder; j++) {
+    result[j]++;
+  }
+
+  return result;
+};
 // ___________________________________________
 
 
