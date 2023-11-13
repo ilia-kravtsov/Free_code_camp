7b52551d74f90a338a23b234ed31bef3e1d320f2
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-11-13 12:38:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7929,22 +7929,75 @@ function translatePigLatin(str) {
 }
 console.log(translatePigLatin("schwartz"))
 translatePigLatin("schwartz");
-// ___________________________________________
+// ___________________________________________ Search and Replace
 
 /*
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
 */
+function myReplace(str, before, after) {
+  let result = str.split(' ')
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].toLowerCase() === before.toLowerCase()) {
+      if (result[i][0] === result[i][0].toUpperCase()) {
+        after = after[0].toUpperCase() + after.slice(1)
+        result[i] = after
+      } else {
+        result[i] = after.toLowerCase()
+      }
+    }
+  }
+  return result.join(' ');
+}
 
-// ___________________________________________
+console.log(myReplace("I think we should look up there", "up", "Down"))
+myReplace("I think we should look up there", "up", "Down")
+// ___________________________________________ DNA Pairing
 
 /*
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
 
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 */
-
-// ___________________________________________
+function pairElement(str) {
+  let result = []
+  for (let i = 0; i < str.length; i++) {
+    switch(str[i]) {
+      case 'G':
+        result.push(['G','C'])
+        break;
+      case 'C':
+        result.push(['C', 'G'])
+        break;
+      case 'A':
+        result.push(['A', 'T'])
+        break;
+      case 'T':
+        result.push(['T', 'A'])
+        break;
+    }
+  }
+  return result;
+}
+console.log(pairElement("ATCGA"))
+pairElement("GCG");
+// ___________________________________________ Missing letters
 
 /*
+Find the missing letter in the passed letter range and return it.
 
+If all letters are present in the range, return undefined.
 */
 
 // ___________________________________________
