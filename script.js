163707938fd39fc06ce2aa1879d90066e0fc381e
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-11-17 11:51:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8019,16 +8019,33 @@ function fearNotLetter(str) {
 }
 console.log(fearNotLetter("bcdf"))
 fearNotLetter("bcdf");
-// ___________________________________________
+// ___________________________________________ Sorted Union
 
 /*
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
 */
+function uniteUnique(...arr) {
+  const newArr = arr.flat()
+  let result = []
+  for (let i = 0; i < newArr.length; i++) {
+    if (!result.includes(newArr[i])) {
+      result.push(newArr[i])
+    }
+  }
+  return result;
+}
 
-// ___________________________________________
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// ___________________________________________ Convert HTML Entities
 
 /*
-
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 */
 
 // ___________________________________________
