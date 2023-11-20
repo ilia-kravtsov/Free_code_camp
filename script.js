135f41efc56bca37da0a8f8db07017e4ed58add8
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-11-20 19:15:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8047,21 +8047,86 @@ uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 /*
 Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 */
-
-// ___________________________________________
+function convertHTML(str) {
+  let array = [...str]
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === '&') {
+        array[i] = '&amp;'
+    } else if (array[i] === '<') {
+        array[i] = '&lt;'
+    } else if (array[i] === '>') {
+        array[i] = '&gt;'
+    } else if (array[i] === '"') {
+        array[i] = '&quot;'
+    } else if (array[i] === "'") {
+        array[i] = '&apos;'
+    } else if (array[i] === "'") {
+        array[i] = '&apos;'
+    }
+  }
+  return array.join('');
+}
+console.log(convertHTML("Hamburgers < Pizza < Tacos"))
+convertHTML("Dolce & Gabbana");
+// ___________________________________________ Sum All Odd Fibonacci Numbers
 
 /*
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
 
+The first two numbers in the Fibonacci sequence are 0 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first seven numbers of the Fibonacci sequence are 0, 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 */
+function sumFibs(num) {
+  let fiboArr = [0,1]
+  for (let i = 2; i <= num; i++) {
+    let newFiboNumber = fiboArr[i-2] + fiboArr[i-1]
+    if (newFiboNumber <= num) {
+      fiboArr.push(newFiboNumber)
+    }
+  }
+  console.log('creating fibo array: ',fiboArr)
+  const filtered = fiboArr.filter(n => n % 2 !== 0)
+  console.log('after filter: ',filtered)
+  const resultSum = filtered.reduce((acc, el) => acc + el)
+  console.log('after sum: ',resultSum)
+  return resultSum
+}
 
-// ___________________________________________
-
-
-
-// ___________________________________________
+sumFibs(10)
+// ___________________________________________ Sum All Primes
 
 /*
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
 
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+*/
+function sumPrimes(num) {
+  let result = []
+  for (let i = 0; i <= num; i++) {
+    result.push(i)
+  }
+  console.log('sequence: ', result)
+  const isPrime = num => {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return num !== 1;
+  };
+  const filtered = result.filter(n => isPrime(n))
+  console.log('filtered: ', filtered)
+  return filtered.reduce((acc, element) => acc + element);
+}
+
+sumPrimes(10);
+// ___________________________________________ Smallest Common Multiple
+
+/*
+Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+The range will be an array of two numbers that will not necessarily be in numerical order.
+
+For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
 */
 
 // ___________________________________________
