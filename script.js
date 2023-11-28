90a06a049ce31a36e42ea4c227b542a7bb2148af
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-11-28 17:12:33 +0300

    Task 'Map the Debris' in progress

--- script.js
+++ script.js
@@ -8338,11 +8338,53 @@ const Person = function(first, last) {
     lastName = newLast;
   };
 };
-// ___________________________________________
+// ___________________________________________ Map the Debris
 
 /*
+According to Kepler's Third Law, the orbital period  T
+  of two point masses orbiting each other in a circular or elliptic orbit is:
 
+T=2πa3μ−−−√
+
+a
+  is the orbit's semi-major axis
+μ=GM
+  is the standard gravitational parameter
+G
+  is the gravitational constant,
+M
+  is the mass of the more massive body.
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+Get Help
 */
+// fix later
+function orbitalPeriod(arr) {
+  const GM = Math.round(398600.4418);
+  const earthRadius = Math.round(6367.4447);
+
+  function calculateOrbitalPeriod(avgAlt) {
+    const newAlt = Math.round(avgAlt)
+    const axis = earthRadius + newAlt;
+    const period = Math.round(2 * Math.PI * Math.sqrt(Math.pow(axis, 3) / GM));
+    return period;
+  }
+
+  const transformedArr = arr.map(obj => {
+    const orbitalPeriod = calculateOrbitalPeriod(obj.avgAlt);
+    return { name: obj.name, orbitalPeriod };
+  });
+
+  return transformedArr;
+}
+
+orbitalPeriod([{ name: 'sputnik', avgAlt: 35873.5553 }]);
 
 // ___________________________________________
 
